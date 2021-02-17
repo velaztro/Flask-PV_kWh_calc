f484b35766710c51a0f714876a6d753878a90d51
velaztro <68763480+velaztro@users.noreply.github.com>
2021-02-17 04:08:54 -0800

    Location improvement

--- static/script/form.js
+++ static/script/form.js
@@ -1,32 +1,23 @@
-let x = document.getElementById("demo");
-  let lat_form = document.getElementById("lat");
-  let lon_form = document.getElementById("lon");
+let lat_form = document.getElementById("lat");
+let lon_form = document.getElementById("lon");
 
-  function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-      x.innerHTML = "Geolocation is not supported by this browser.";
-    }
+const getLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition,error);
+    lat_form.placeholder = "Locating...";
+    lon_form.placeholder = "Locating...";
+  } else {
+    lat_form.placeholder = "Geolocation is not supported by this browser.";
+    lon_form.placeholder = "Geolocation is not supported by this browser.";
   }
+}
 
-  function showPosition(position) {
-    let lat = position.coords.latitude;
-    let long = position.coords.longitude;
-    lat_form.value = lat.toFixed(4);
-    lon_form.value = long.toFixed(4);
+const showPosition = (position) => {
+  lat_form.value = position.coords.latitude.toFixed(4);
+  lon_form.value = position.coords.longitude.toFixed(4);
+}
 
-    /*fetch('/sign-up', {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      method: 'POST',
-
-      body: JSON.stringify({
-          "lat": lat,
-          "long": long
-        })
-      }).then(function (response) { // At this point, Flask has printed our JSON
-        return response.text();
-      });*/
+function error() {
+  lat_form.placeholder = "Geolocation disabled";
+  lon_form.placeholder = "Geolocation disabled";
 }
